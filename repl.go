90c6a4ffa61fe01f6dab449ec15b159ea92f526e
314159otr <piotrbpersonal@gmail.com>
2026-05-23 15:20:06 +0200

    CH1: REPL - L4: REPL

--- repl.go
+++ repl.go
@@ -1,6 +1,26 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		clean := cleanInput(input)
+		firstWord := ""
+		if len(clean) > 0 {
+			firstWord = clean[0]
+		}
+		fmt.Println("Your command was: " + firstWord)
+	}
+}
 
 func cleanInput(text string) []string {
 	lower := strings.ToLower(text)
